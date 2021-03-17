c18c7d1ee6bc1c68171203efa3ed8dc0f06919d8
José Castro <jecdegois12345@gmail.com>
2021-03-17 15:51:40 -0400

    Pugstagram ready

---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
+<div class="Footer">
+  <div class="Footer-container">
+    <div class="Footer-copy">
+      <p>
+        2021© Pugstagram
+      </p>
+    </div>
+  </div>
+</div>
